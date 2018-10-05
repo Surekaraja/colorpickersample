edbdb1fc40e3b73bb301877f87b806ebbed5d790
Surekaraja <43601870+Surekaraja@users.noreply.github.com>
2018-10-05 17:20:41 +0530

    v
    dgf

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
   {
     super(props);
     this.state = {
-      defaultValue: '#ooooo'      
+      defaultValue: '#ffff'      
     };    
     this.handleChangeComplete =this.handleChangeComplete.bind(this);   
   }  
